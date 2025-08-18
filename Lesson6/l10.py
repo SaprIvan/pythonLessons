e8a11dc0ce0c9b106d6fe3d13c0f9239a3105411
ivan_s <sapron.ivan@gmail.com>
2025-08-18 14:44:04 +0300

    fix l10

--- Lesson6/l10.py
+++ Lesson6/l10.py
@@ -16,17 +16,24 @@ class Colors:
         return cls.__default
 
 class HealthBar:
-    __remaining_health_symbol: '█'
-    __lost_health_symbol: '_'
-    __bars_count: 20
+    __remaining_health_symbol = '█'
+    __lost_health_symbol = '_'
+    __bars_count = 20
 
     def __init__(self, max_health, bar_color):
-        self.__bar_color = bar_color
-        self.__max_health = max_health
+        self.bar_color = bar_color
+        self.max_health = max_health
 
     def draw(self, hp):
-        Colors.green()
-        return f"|{Цвет окраски}{символы оставшегося хп}{символы утерянного хп}{Дефолтный цвет}|"
+        __health = ''
+        __health_lost = ''
+        remaining_health_bars = int(round(hp/self.max_health*self.__bars_count,0))
+        for _ in range(self.__bars_count):
+            if len(__health) < remaining_health_bars:
+                __health+=self.__remaining_health_symbol
+            else:
+                __health_lost += self.__lost_health_symbol
+        return f"|{self.bar_color}{ __health}{__health_lost}{Colors.default()}|"
 
 
 if __name__ == '__main__':
