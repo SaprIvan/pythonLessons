eb3c47e2000e73655a1fa0ad1cd47f6c6b2efe6e
ivan_s <sapron.ivan@gmail.com>
2025-09-03 12:20:30 +0300

    rebase classes

--- Classes_tasks_final/engine/animation.py
+++ Classes_tasks_final/engine/animation.py
@@ -5,6 +5,26 @@ from engine.events import animation_events
 
 
 # Место для реализации классов TextAnimation и AnimationController
+@dataclass
+class TextAnimation:
+    states: list[any] = field(default_factory=list)
+    attempts: int = 1
+    interval: float = 3.0
+
+    def play(self):
+        animation_events.Start(self.states, self.attempts, self.interval)
+
+class AnimationController:
+    def __init__(self):
+        animation_events.Start += self.animation
+
+    def animation(self, states: list[any], attempts: int, interval: float):
+        for attempt in range(attempts):
+            for state in states:
+                print(state, end="")
+                sleep(interval)
+                print("\r", end="")
+        print(end="\n")
 
 
 
--- Classes_tasks_final/engine/configs.py
+++ Classes_tasks_final/engine/configs.py
@@ -1,4 +1,16 @@
 from dataclasses import dataclass
+from typing import List
 
 
-# Место для реализации класса GameConfig
+@dataclass
+class GameConfig:
+    name: str
+    pattern: List[List[str]]
+
+
+    def  __post_init__(self):
+        self.map_size_x: int = len(self.pattern)
+        self.map_size_y: int = len(self.pattern[0])
+
+    def __str__(self):
+        return f'[{self.name}] Размер карты: {self.map_size_x}x{self.map_size_y}'
