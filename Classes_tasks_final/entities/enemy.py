2561a4617e95567174743885f51f06615df7f8bd
ivan_s <sapron.ivan@gmail.com>
2025-09-03 14:33:47 +0300

    upd

--- Classes_tasks_final/entities/enemy.py
+++ Classes_tasks_final/entities/enemy.py
@@ -5,3 +5,12 @@ from entities.items import Armor, Weapon
 
 
 # Место для реализации класса Enemy
+
+class Enemy(Entity):
+    def __init__(self, armor: Armor, weapon: Weapon, data: EntityData):
+        __tag = "Enemy"
+        super().__init__(__tag, data, weapon, armor)
+        self._hp_bar.bar_color = Colors.red
+
+    def die(self):
+        self._is_dead = True
--- Classes_tasks_final/entities/player.py
+++ Classes_tasks_final/entities/player.py
@@ -1,3 +1,4 @@
+from dungeon.cell_entity import Cell
 from engine.events import entity_events
 from engine.visual import CellType, Colors
 from entities.entity import Entity, EntityItemTypeError
@@ -9,11 +10,18 @@ from entities.items import Weapon, Armor
 class Player(Entity):
     def __init__(self, armor: Armor, weapon: Weapon, data: EntityData):
         # Место для дополнения класса Player атрибутами и вызовом инициализатора родительского класса
-
+        __tag = "Player"
+        super().__init__(__tag, data, weapon, armor)
+        self._hp_bar.bar_color = Colors.green
+        self.__location = None
+        self.__inventory = Inventory()
         entity_events.Change_Armor += self.change_armor
         entity_events.Change_Weapon += self.change_weapon
 
     # Место для дополнения класса Player свойствами
+    @property
+    def location(self):
+        return self.__location
 
     @location.setter
     def location(self, value):
@@ -31,4 +39,41 @@ class Player(Entity):
         self.__location = value
         self.__location.cell_type = CellType.player
 
-    # Место для дополнения класса Player методами
+    @property
+    def inventory(self):
+        return self.__inventory
+
+    def move(self, cell: Cell):
+        self.__location = cell
+
+    def die(self):
+        self._is_dead = True
+
+    def change_weapon(self, weapon: Weapon):
+        if isinstance(weapon, Weapon):
+            self.__inventory.add_item(self.weapon)
+            self.weapon = weapon
+        else:
+            raise EntityItemTypeError
+
+    def change_armor(self, armor: Armor):
+        if isinstance(armor, Armor):
+            self.__inventory.add_item(self.armor)
+            self.armor = armor
+        else:
+            raise EntityItemTypeError
+
+    def lift_item(self):
+        if hasattr(self.__location, "item") and self.__location.item is not None:
+            self.__inventory.add_item(self.__location.item)
+            entity_events.Lift_Item(self.__location.item)
+            self.__location.item = None
+
+    def looting_dead_enemy(self):
+        if hasattr(self.__location, "entity") and self.__location.entity is not None and self.__location.entity.is_dead:
+            self.__inventory.add_item(self.__location.entity.armor)
+            self.__inventory.add_item(self.__location.entity.weapon)
+            self.__location.entity.weapon = None
+            self.__location.entity.armor = None
+            entity_events.Lift_Item(self.__location.entity.weapon)
+            entity_events.Lift_Item(self.__location.entity.armor)
